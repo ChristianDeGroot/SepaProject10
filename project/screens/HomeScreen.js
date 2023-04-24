6456cd0491720c541740adf489009a953980fd43
ChristianDeGroot <104880638+ChristianDeGroot@users.noreply.github.com>
2023-04-24 17:49:09 +1000

    image display

--- project/screens/HomeScreen.js
+++ project/screens/HomeScreen.js
@@ -1,7 +1,10 @@
 // screens/HomeScreen.js
 
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, Image, FileSystem } from 'react-native'; 
+//import myImage from 'assets/download.jpg';
+//<Image source={myImage} />
+//import { FileSystem } from 'react-native';
 
 const HomeScreen = ({ navigation }) => {
   return (
@@ -11,6 +14,13 @@ const HomeScreen = ({ navigation }) => {
         <Button
           title="Logout"
           onPress={() => navigation.navigate('Login')}
+        />
+        <Text>Importing image from the internet:</Text>
+        <Image source={{url: 'https://www.google.com.au/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3AJPG_Test.jpg&psig=AOvVaw1k3eTq5kGxjyb_VZOzYDQE&ust=1682407471823000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCOjjp6_-wf4CFQAAAAAdAAAAABAE'}} />
+        <Text>Importing image from local computer:</Text>
+        
+        <Image 
+          source={require('../assets/download.jpg')}  
         />
       </View>
     </View>
